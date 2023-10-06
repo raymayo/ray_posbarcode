36242d6de2f320e53922d4dec8cee327d3e3818c
Ray Mayo <ormillada.jr@gmail.com>
2023-10-06 20:47:30 +0800

    tuts 14 finished

--- index.php
+++ index.php
@@ -1,35 +1,36 @@
 <?php
 
-include_once"ui/connectdb.php";
+include_once "ui/connectdb.php";
 
 session_start();
 
-if(isset($_POST['btn_login'])){
-  
-    $useremail = $_POST['txt_email'];
-    $password = $_POST['txt_password'];
+if (isset($_POST['btn_login'])) {
+
+  $useremail = $_POST['txt_email'];
+  $password = $_POST['txt_password'];
 
 
-    $select = $pdo->prepare("select * from tbl_form where useremail='$useremail' AND userpassword='$password'");
-    $select->execute();
+  $select = $pdo->prepare("select * from tbl_form where useremail='$useremail' AND userpassword='$password'");
+  $select->execute();
 
-    $row = $select->fetch(PDO::FETCH_ASSOC);
+  $row = $select->fetch(PDO::FETCH_ASSOC);
 
-    if($row['useremail']==$useremail AND $row['userpassword']==$password){
+  if (is_array($row)) {
 
-      echo $success="Login Success";
+    if ($row['useremail'] == $useremail and $row['userpassword'] == $password) {
+      echo $success = "Login Success";
       header('refresh: 1;ui/dashboard.php');
-    }else{
-
-      echo $success="Wrong Email or Password";
-
     }
+  } else {
+    echo $success = "Wrong Email or Password";
+  }
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -44,68 +45,70 @@ if(isset($_POST['btn_login'])){
   <!-- Theme style -->
   <link rel="stylesheet" href="./dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="./index2.html" class="h1"><b>POS</b>BARCODE</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+  <div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+      <div class="card-header text-center">
+        <a href="./index2.html" class="h1"><b>POS</b>BARCODE</a>
+      </div>
+      <div class="card-body">
+        <p class="login-box-msg">Sign in to start your session</p>
 
-      <form action="" method="post">
-        <div class="input-group mb-3">
+        <form action="" method="post">
+          <div class="input-group mb-3">
 
-          <input type="email" class="form-control" placeholder="Email" name='txt_email'>
+            <input type="email" class="form-control" placeholder="Email" name='txt_email'>
 
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
+          <div class="input-group mb-3">
 
-          <input type="password" class="form-control" placeholder="Password" name="txt_password">
+            <input type="password" class="form-control" placeholder="Password" name="txt_password">
 
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
+          <div class="row">
+            <div class="col-8">
+              <div class="icheck-primary">
                 <a href="forgot-password.html">I forgot my password</a>
+              </div>
             </div>
+            <!-- /.col -->
+            <div class="col-4">
+              <button type="submit" class="btn btn-primary btn-block" name="btn_login">Login</button>
+            </div>
+            <!-- /.col -->
           </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block" name="btn_login">Login</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
+        </form>
 
-      <!-- /.social-auth-links -->
+        <!-- /.social-auth-links -->
 
-      <p class="mb-1">
+        <p class="mb-1">
 
-      </p>
+        </p>
+      </div>
+      <!-- /.card-body -->
     </div>
-    <!-- /.card-body -->
+    <!-- /.card -->
   </div>
-  <!-- /.card -->
-</div>
-<!-- /.login-box -->
+  <!-- /.login-box -->
 
-<!-- jQuery -->
-<script src="./plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="./dist/js/adminlte.min.js"></script>
+  <!-- jQuery -->
+  <script src="./plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="./dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
