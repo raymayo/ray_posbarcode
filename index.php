73ee854d480dced693b6eaee7bea31e53f51ef70
Ray Mayo <ormillada.jr@gmail.com>
2023-10-23 13:39:19 +0800

    changed title

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if (isset($_POST['btn_login'])) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in moko</title>
+  <title>AdminLTE 3 | Log in moto</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
